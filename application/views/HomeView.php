b4746a354b7551fe66b13ef0909e0ba54981027c
WimpyPotato <javenjack@gmail.com>
2017-04-04 00:36:45 -0400

    Project (xpt system and theme folder)
    too many files..

---
+++ application/views/HomeView.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Crippling Depression</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/theme/css/foundation.min.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/theme/css/normalize.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/theme/css/styles.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/theme/css/assets.css" />
+    </head>
+
+    <body>
+        <div class="top"> 
+            <h2>
+                penis
+            </h2>
+        </div>
+        <h1><marquee behavior="slide" direction="left">i like tits..........................................................but i like butts more</marquee></h1>
+        <img src="<?php echo base_url(); ?>/theme/assets/753.png" width="300" height="300" alt="753">
+        <div class="top"><h1>GO SUCK YO MOTHA</h1></div>
+        <style style="text/css">
+.example1 {
+ height: 50px;	
+ overflow: hidden;
+ position: relative;
+}
+.example1 h3 {
+ font-size: 3em;
+ color: limegreen;
+ position: absolute;
+ width: 100%;
+ height: 100%;
+ margin: 0;
+ line-height: 50px;
+ text-align: center;
+ /* Starting position */
+ -moz-transform:translateX(100%);
+ -webkit-transform:translateX(100%);	
+ transform:translateX(100%);
+ /* Apply animation to this element */	
+ -moz-animation: example1 5s linear infinite;
+ -webkit-animation: example1 5s linear infinite;
+ animation: example1 5s linear infinite;
+}
+/* Move it (define the animation) */
+@-moz-keyframes example1 {
+ 0%   { -moz-transform: translateX(100%); }
+ 100% { -moz-transform: translateX(-100%); }
+}
+@-webkit-keyframes example1 {
+ 0%   { -webkit-transform: translateX(100%); }
+ 100% { -webkit-transform: translateX(-100%); }
+}
+@keyframes example1 {
+ 0%   { 
+ -moz-transform: translateX(100%); /* Firefox bug fix */
+ -webkit-transform: translateX(100%); /* Firefox bug fix */
+ transform: translateX(100%); 		
+ }
+ 100% { 
+ -moz-transform: translateX(-100%); /* Firefox bug fix */
+ -webkit-transform: translateX(-100%); /* Firefox bug fix */
+ transform: translateX(-100%); 
+ }
+}
+</style>
+<div class="example1">
+<h3>PEARS!!!</h3>
+</div>
+    </body>
+</html>
